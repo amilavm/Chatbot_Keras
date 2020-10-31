a13747c0d9ade4030105bbcf59f5a3eab1769bd7
Amila Viraj <amilaviraj@Amilas-MacBook-Pro.local>
2020-10-31 22:36:55 +0530

    added initial source files

---
+++ chat.py
@@ -0,0 +1,49 @@
+import json 
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+
+import colorama 
+colorama.init()
+from colorama import Fore, Style, Back
+
+import random
+import pickle
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+
+def chat():
+    # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+    
+    while True:
+        print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
+        inp = input()
+        if inp.lower() == "quit":
+            break
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data['intents']:
+            if i['tag'] == tag:
+                print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + Style.RESET_ALL)
+chat()
---
+++ requirements.txt
@@ -0,0 +1,6 @@
+tensorflow==2.3.1
+nltk==3.5
+colorama==0.4.3
+numpy==1.18.5
+scikit_learn==0.23.2
+Flask==1.1.2
